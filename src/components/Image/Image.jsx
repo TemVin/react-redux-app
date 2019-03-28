9e121e9f2f40777be9a7eb83a2ab536b049e6f69
Artem Vinogradov <Artemon.waa@yandex.ru>
2019-03-28 16:31:17 +0300

    added styles
    -- added styles for images and remove extra picture

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -4,11 +4,12 @@ import PropTypes from 'prop-types';
 
 import styles from './Image.less';
 
-const Image = ({ url, alt }) => (
-  <img src={url} alt={alt} />
+const Image = ({ className, url, alt }) => (
+  <img className={className} src={url} alt={alt} />
 );
 
 Image.propTypes = {
+  className: PropTypes.string,
   url: PropTypes.string,
   alt: PropTypes.string,
 };
